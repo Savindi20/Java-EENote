5a35ab435039b5cbdb8003577679a2784f5fe5f4
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-08-03 17:58:02 +0530

    third Module added

---
+++ 03_SimpleApp/src/CustomerServlet.java
@@ -0,0 +1,22 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/customer")
+public class CustomerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Customer Saved");
+        String id = req.getParameter("id");
+        String name = req.getParameter("name");
+        String address = req.getParameter("address");
+        String salary = req.getParameter("salary");
+        System.out.println(id+" "+name+" "+address+" "+salary);
+        PrintWriter writer = resp.getWriter();
+        writer.write("<h1>hello there</h1>");
+    }
+}
