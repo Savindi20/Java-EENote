154ccee86217aff8598d36889553cfb1f9b33a6d
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-08-08 17:16:13 +0530

    multiple,wildCard mapping added

--- 05_Mapping-spec/src/servlet/MultipleMapping.java
+++ 05_Mapping-spec/src/servlet/MultipleMapping.java
@@ -7,10 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/item/*") // http://localhost:8080/05_mapping/item http://localhost:8080/05_mapping/item/I001/lux
-public class WildCardMapping extends HttpServlet {
+@WebServlet(urlPatterns = {"/a","/b","/c"})
+public class MultipleMapping extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("Wild Card Mapping Invoked");
+        resp.getWriter().write("Multiple Mapping Invoked A/ B/ C");
     }
 }
