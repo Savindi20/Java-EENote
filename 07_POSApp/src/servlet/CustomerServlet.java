9b68f85532a967b165db4c0389236e6807bdad44
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-08-10 10:25:48 +0530

    7 pos appadded

---
+++ 07_POSApp/src/servlet/CustomerServlet.java
@@ -0,0 +1,82 @@
+package servlet;
+
+import dto.CustomerDTO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(urlPatterns = "/Customer")
+public class CustomerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<CustomerDTO> allCustomers = new ArrayList<>();
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/posdb", "root", "1234");
+            PreparedStatement pstm = connection.prepareStatement("SELECT * FROM Customer");
+            ResultSet rst = pstm.executeQuery();
+            while (rst.next()) {
+                String id = rst.getString("id");
+                String name = rst.getString("name");
+                String address = rst.getString("address");
+                double salary = rst.getDouble("salary");
+                allCustomers.add(new CustomerDTO(id, name, address, salary));
+            }
+//            resp.sendRedirect("customer.jsp");
+            req.setAttribute("customers",allCustomers);
+            req.getRequestDispatcher("customer.jsp").forward(req,resp);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+        String name = req.getParameter("name");
+        String address = req.getParameter("address");
+        double salary = Double.parseDouble(req.getParameter("salary"));
+        String option=req.getParameter("option");
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/posdb", "root", "1234");
+
+            if(option.equals("add")){
+
+                PreparedStatement pstm = connection.prepareStatement("insert into Customer values(?,?,?,?)");
+                pstm.setObject(1,id);
+                pstm.setObject(2,name);
+                pstm.setObject(3,address);
+                pstm.setObject(4,salary);
+                boolean b = pstm.executeUpdate() > 0;
+//            PrintWriter writer = resp.getWriter();
+//            writer.write("<h1>Customer added state : "+b+"</h1>");
+
+            }else if(option.equals("remove")){
+                PreparedStatement pstm = connection.prepareStatement("delete from Customer where id=?");
+                pstm.setObject(1,id);
+                boolean b = pstm.executeUpdate() > 0;
+            }else if(option.equals("update")){
+                PreparedStatement pstm = connection.prepareStatement("update Customer set name=?,address=?,salary=? where id=?");
+                pstm.setObject(4,id);
+                pstm.setObject(1,name);
+                pstm.setObject(2,address);
+                pstm.setObject(3,salary);
+                boolean b = pstm.executeUpdate() > 0;
+            }
+
+            resp.sendRedirect("Customer");
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
