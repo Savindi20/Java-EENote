b63c894c420572563efd3dd8dc6b394554e6e14b
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-08-26 00:08:49 +0530

    default filter updated

--- 10_Filters/src/Filters/DefaultFilter.java
+++ 10_Filters/src/Filters/DefaultFilter.java
@@ -2,6 +2,7 @@ package Filters;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @WebFilter(urlPatterns = "/*")
@@ -13,7 +14,21 @@ public class DefaultFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("Default Filter Invoked");
+        //http://localhost:8081/filter/a
+        //a?name=ijse ---> dispatch a
+        //b?name=ijse ---> dispatch b
+        String name = servletRequest.getParameter("name");
+
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        String servletPath = req.getServletPath();
+        System.out.println(name+" "+servletPath);
+        if(name.equals("ijse")&&servletPath.equals("/a")){
+            filterChain.doFilter(servletRequest,servletResponse);
+        }else if (name.equals("ijse")&&servletPath.equals("/b")){
+            filterChain.doFilter(servletRequest,servletResponse);
+        }else {
+            servletResponse.getWriter().write("<h1>Case</h1>");
+        }
     }
 
     @Override
