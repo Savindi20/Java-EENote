9923880f724fb2e6fae12141723fcf8fae9e2c51
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-09-04 14:31:00 +0530

    listeners updated

--- 12_Listeners/src/servlet/ServletA.java
+++ 12_Listeners/src/servlet/ServletA.java
@@ -1,11 +1,30 @@
 package servlet;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(urlPatterns = "/a")
 public class ServletA extends HttpServlet {
     public ServletA(){
         System.out.println("Servlet A Instantiated");
+    }
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("Servlet A Init");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("servlet A doGet Method Invoked");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Servlet A Destroy");
     }
 }
--- 12_Listeners/src/servlet/ServletB.java
+++ 12_Listeners/src/servlet/ServletB.java
@@ -1,11 +1,30 @@
 package servlet;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(urlPatterns = "/b")
 public class ServletB extends HttpServlet {
     public ServletB(){
         System.out.println("Servlet B Instantiated");
+    }
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("Servlet B Init");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("servlet B doGet Method Invoked");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Servlet B Destroy");
     }
 }
