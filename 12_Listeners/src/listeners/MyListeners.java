c5e207e7222954e329452e7ae422d04a9290c57a
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-09-07 10:48:07 +0530

    content updated

--- 12_Listeners/src/listeners/MyListeners.java
+++ 12_Listeners/src/listeners/MyListeners.java
@@ -1,5 +1,6 @@
 package listeners;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
@@ -9,6 +10,8 @@ public class MyListeners implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         System.out.println("Servlet Context created");
+        ServletContext servletContext = servletContextEvent.getServletContext();
+        servletContext.setAttribute("skey", "SAVINDI");
     }
 
     @Override
--- 12_Listeners/src/servlet/ServletA.java
+++ 12_Listeners/src/servlet/ServletA.java
@@ -1,5 +1,6 @@
 package servlet;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,6 +22,9 @@ public class ServletA extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("servlet A doGet Method Invoked");
+        ServletContext servletContext = getServletContext();
+        String value = (String) servletContext.getAttribute("skey");
+        System.out.println(value);
     }
 
     @Override
